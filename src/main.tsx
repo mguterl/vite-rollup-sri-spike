2562ffe0e9a2aea39e42f1b012143dd3b16f0a76
Mike Guterl <mike@diminishing.org>
2024-11-10 21:23:52 -0800

    Basic app

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import React, { lazy, Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Home from './routes/Home';
+import About from './routes/About';
+
+const Contact = lazy(() => import('./routes/Contact'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: 'about', element: <About /> },
+      {
+        path: 'contact',
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Contact />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/Contact.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div>
+      <h2>Contact Page</h2>
+      <p>Get in touch with us here.</p>
+    </div>
+  );
+}
